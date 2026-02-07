032818ef233d3f91cc5464ef94ac0582eca1ac4e
princemkhwanazijr-blip <princemkhwanazijr@gmail.com>
2026-02-07 16:38:58 +0200

    Delete gh-create-and-push.sh

--- gh-create-and-push.sh
+++
@@ -1,4 +0,0 @@
-# requires GitHub CLI (gh) already installed and authenticated (gh auth login)
-# from project root
-gh repo create princemkhwanazijr-blip/<repo> --public --source=. --remote=origin --push
-# That will create the GitHub repo, add origin remote, and push the current branch.
